09cfab45bcfb30a7853ea5d894c71da8f203c6a4
Rochak Saini <rochaks001@gmail.com>
2022-02-11 22:49:25 +0530

    Update inference_api.py

--- codes/inference_api.py
+++ codes/inference_api.py
@@ -8,7 +8,7 @@ from openvino.inference_engine import IECore
 from torchvision import transforms
 
 config = {'model_path': '/mnist_vino/mnist.xml'}
-
+transform = transforms.ToTensor()
 app = FastAPI()
 
 def load_model():
@@ -28,7 +28,6 @@ async def predict(image: UploadFile = File(...)):
 	pil_image = pil_image.resize((28, 28))
 	np_img = np.asarray(pil_image)
 	np_img = cv2.cvtColor(np_img, cv2.COLOR_BGR2GRAY)/255
-	transform = transforms.ToTensor()
 	tensor = transform(np_img).unsqueeze(0)
 	res_ir = net[0].infer(inputs={net[1]:tensor})
 	res_ir = res_ir[net[2]]
